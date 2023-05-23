da59767324d2dfa6dba9f5a66e39a56c88e1326c
issufibadji <issufibadji@gmail.com>
2023-05-23 12:59:11 -0300

    add1

---
+++ reactjs/projeto-01-reactjs/src/App.jsx
@@ -0,0 +1,14 @@
+//todos componentes de js tem extensão jsx
+//jsx=js+xml
+import Post from './Post';
+
+function App() {
+  return (
+  <div>
+  <Post/>
+ </div>
+  
+  )
+}
+
+export default App
---
+++ reactjs/projeto-01-reactjs/src/Post.jsx
@@ -0,0 +1,4 @@
+function Post() { 
+  return <h1>Hello Post</h1>
+}
+export default Post
