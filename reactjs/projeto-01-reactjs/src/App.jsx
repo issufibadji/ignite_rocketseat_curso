5052466265348d50c96c4757a6e40df0913e7450
trf/conibj <issufi.badji@tty2000.com.br>
2023-05-24 15:41:18 -0300

    Estrutura da aplicação:CSS Models

--- reactjs/projeto-01-reactjs/src/App.jsx
+++ reactjs/projeto-01-reactjs/src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from "./Post";
+import { Header } from "./assets/components/Header";
+import './styles.css';
 
 export function App() {
   return(
     <div>
+      <Header />
+
       <Post 
       author ="Issufi Badji" 
       content ="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda impedit repellendus ipsa porro, provident dolor accusamus, architecto unde sit nobis odio distinctio iure eos iusto modi earum facilis necessitatibus harum."
